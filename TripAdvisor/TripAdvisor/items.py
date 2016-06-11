33df660fef04fdc06cd3b4f7b705617487f446ce
bkawan <bikeshkawang@gmail.com>
2016-06-11 08:20:56 +0545

    spiders updateed

--- TripAdvisor/TripAdvisor/items.py
+++ TripAdvisor/TripAdvisor/items.py
@@ -11,25 +11,42 @@ import scrapy
 class TripadvisorItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    title = scrapy.Field()
+    PlaceTitle = scrapy.Field()
+    PlaceURL = scrapy.Field()
+    TotalReviews = scrapy.Field()
+    PlaceCategory = scrapy.Field()
+    StreetAddress = scrapy.Field()
+    AddressLocality = scrapy.Field()
+    AddressRegion = scrapy.Field()
+    PostCode = scrapy.Field()
+    PhoneNumber = scrapy.Field()
+    Ranking = scrapy.Field()
+    LengthOfVisit = scrapy.Field()
+    Fee = scrapy.Field()
+    Description = scrapy.Field()
+    AverageRating = scrapy.Field()
+    OpeningHours = scrapy.Field()
 
-    link = scrapy.Field()
-    review = scrapy.Field()
+    Email = scrapy.Field()
+    Website = scrapy.Field()
 
-    # address = scrapy.Field()
-
-    street_address = scrapy.Field()
-    address_locality = scrapy.Field()
-    address_region = scrapy.Field()
-    postal_code = scrapy.Field()
-
-    # email = scrapy.Field()
-    phone = scrapy.Field()
-    rank = scrapy.Field()
-
-    state_attraction_link = scrapy.Field()
-    state_name = scrapy.Field()
-    state_attraction_name = scrapy.Field()
+    Longitude = scrapy.Field()
+    Latitude = scrapy.Field()
 
 
+    # state_attraction_link = scrapy.Field()
+    # state_name = scrapy.Field()
+    # state_attraction_name = scrapy.Field()
+
+
+class TripReviewItem(scrapy.Item):
+    LocationTitle = scrapy.Field()
+    Username = scrapy.Field()
+    UserImageURL = scrapy.Field()
+    ReviewPictureURL = scrapy.Field()
+    Rating = scrapy.Field()
+    CommentTitle = scrapy.Field()
+    CommentDate = scrapy.Field()
+    Comment = scrapy.Field()
+    LocationURL = scrapy.Field()
 
--- TripAdvisor/TripAdvisor/spiders/tripadvisor.py
+++ TripAdvisor/TripAdvisor/spiders/tripadvisor.py
@@ -7,74 +7,176 @@ from scrapy import Selector
 class TripadvisorSpider(scrapy.Spider):
     name = "tripadvisor"
     allowed_domains = ["tripadvisor.com"]
+    start_urls = [
+        "https://www.tripadvisor.com/Attractions-g191-Activities-United_States.html",
+        # "https://www.tripadvisor.com/Attractions-g34345-Activities-oa30-Key_West_Florida_Keys_Florida.html"
+    ]
 
-    i = 0
-    increment = 20
-    first_link ="https://www.tripadvisor.com/Attractions-g191-Activities-United_States.html"
-    start_urls = []
+    def parse(self, response):
 
-    # loop through all pagination link from location list
-    while i < 333:
+        sel = Selector(response)
+        print("************** First **************************")
 
-        link = "https://www.tripadvisor.com/Attractions-g191-Activities-oa{}-United_States.html#LOCATION_LIST".format(
-        str(increment))
-        start_urls.append(link)
-        increment +=50
-        i +=1
+        subject = sel.xpath('//div[@class="property_title"]/a/text()').extract()
+        links = sel.xpath('//div[@class="property_title"]/a/@href').extract()
 
-    def parse(self,response):
-        '''
-        parse  each link from the location list and callback parse_attraction_list
-        '''
-        for sel in response.xpath("//ul[@class='geoList']"):
-            location_link = sel.xpath("li/a/@href").extract()
-            links = []
-            for href in location_link:
-                location_link = response.urljoin(href)
-                links.append(location_link)
+        for link in links:
+            link = response.urljoin(link)
+            yield scrapy.Request(link, callback=self.parse_each_attraction)
 
-                yield scrapy.Request(location_link, callback=self.parse_attraction_list)
+        sites = sel.xpath('//a[contains(text(), "Next")]/@href').extract()
+        for si in sites:
+            if si:
+                ss = response.urljoin(si)
+                yield scrapy.Request(ss, callback=self.parse_location)
 
-    def parse_attraction_list(self, response):
-        '''
-        parse each attraction link from list of attractions from each  location and callback parse_attraction_contents
-        '''
-        for href in response.xpath("//div[@class='property_title']/a/@href").extract():
-            url = response.urljoin(href)
-            print("I am here"+url)
-            yield scrapy.Request(url, callback=self.parse_attraction_contents)
-    #
+        print("************** First **************************")
 
-    def parse_attraction_contents(self, response):
-        '''
-        parse each attraction  to extract its contents
-        '''
-        for select in response.xpath('//span[@class="format_address"]'):
+    def parse_location(self, response):
+        sel = Selector(response)
+        print(" **************** LOCATION LIST *************")
+        print(response.url)
+        print(" **************** LOCATION LIST *************")
 
-            trip_item = TripadvisorItem()# instantiate TripadvisorItem to store data extracted
-            trip_item['link'] = response.url
+        location = sel.xpath("//ul[@class='geoList']")
+        for loc in location:
+            state_link = loc.xpath("li/a/@href").extract()
+            print(" **************** Attraction List starts *************")
 
-            trip_item['street_address'] = select.xpath('span[@class="street-address"]/text()').extract() + select.xpath('span[@class="extended-address"]/text()').extract()
+            for link in state_link:
+                url_link = response.urljoin(link)
+                print(url_link)
+                # "https://www.tripadvisor.com/Attractions-g34345-Activities-Key_West_Florida_Keys_Florida.html"
+                yield scrapy.Request(url_link, callback=self.parse_attraction)
+            print(" **************** Attraction List  ends *************")
 
-            trip_item['address_locality'] = select.xpath('span[@class="locality"]/span[@property="addressLocality"]/text()').extract()
-            trip_item['address_region'] = select.xpath('span[@class="locality"]/span[@property="addressRegion"]/text()').extract()
-            trip_item['postal_code'] = select.xpath('span[@class="locality"]/span[@property="postalCode"]/text()').extract()
-            trip_item['title']=  response.xpath("//h1[@id='HEADING']/text()")[1].extract().strip()
 
-            trip_item['review'] = response.xpath('//a[@class="more"]/text()').extract()
-            div = response.xpath("//div[@class='slim_ranking']").extract()
+
+            # yield scrapy.Request(url_link,callback=self.parse_test)
+
+        locations = sel.xpath("//a[@class='guiArw sprite-pageNext  pid0']/@href").extract()
+        print(" **************** LOCATION LIST  PAGINATION  starts *************")
+        print(locations)
+        print(" **************** LOCATION Link *************")
+
+        for location in locations:
+            if location:
+                location_link = response.urljoin(location)
+                print(location_link)
+                yield scrapy.Request(location_link, callback=self.parse_location)
+        print(" **************** LOCATION Link *************")
+
+        print(" **************** LOCATION LIST  PAGINATION  ends *************")
+
+    def parse_attraction(self, response):
+
+        print(" **************** Attraction URL reponse *************")
+        print(response.url)
+        print(" **************** Attraction URL reponse *************")
+
+        sel = Selector(response)
+        subject = sel.xpath('//div[@class="property_title"]/a/text()').extract()
+        links = sel.xpath('//div[@class="property_title"]/a/@href').extract()
+
+        print(" **************** Attraction URL List starts *************")
+
+        for link in links:
+            link = response.urljoin(link)
+            print(link)
+
+            yield scrapy.Request(link, callback=self.parse_each_attraction)
+        print(" **************** Attraction URL List ends *************")
+
+        sites = sel.xpath('//a[contains(text(), "Next")]/@href').extract()
+        for si in sites:
+            if si:
+                ss = response.urljoin(si)
+                yield scrapy.Request(ss, callback=self.parse_attraction)
+
+    def parse_each_attraction(self, response):
+        sel = Selector(response)
+        # print("********")
+        # print(response.url)
+        # print("********")
+
+        trip_item = TripadvisorItem()
+        title = response.xpath("//h1[@id='HEADING']/text()").extract()
+        place_title = ""
+        for t in title:
+            place_title += t
+        trip_item['PlaceTitle'] = place_title.strip()
+
+        trip_item['PlaceURL'] = response.url
+
+        if len(response.xpath('//a[@class="more"]/text()').extract()) > 0:
+            total_reviews = response.xpath('//a[@class="more"]/text()').extract()[0].split("R")[0]
+            trip_item['TotalReviews'] = total_reviews
+        else:
+            trip_item['TotalReviews'] = ""
+        # total_reviews = response.xpath('//a[@class="more"]/text()').extract()[0].split("R")[0]
+
+
+        #
+        if len(response.xpath("//div[@class='separator']").extract()) > 0:
+            placeCatDiv = response.xpath("//div[@class='separator']").extract()
+            placeCatSel = Selector(text=placeCatDiv[0])
+            trip_item['PlaceCategory'] = placeCatSel.xpath("string(//div[1])").extract_first().strip()
+        else:
+            trip_item['PlaceCategory'] = ""
+
+        trip_item['StreetAddress'] = response.xpath(
+            '//span[@class="street-address"]/text()').extract() + response.xpath(
+            'span[@class="extended-address"]/text()').extract()
+        trip_item['AddressLocality'] = response.xpath(
+            '//span[@class="locality"]/span[@property="addressLocality"]/text()').extract()
+        trip_item['AddressRegion'] = response.xpath(
+            '//span[@class="locality"]/span[@property="addressRegion"]/text()').extract()
+        trip_item['PostCode'] = response.xpath(
+            '//span[@class="locality"]/span[@property="postalCode"]/text()').extract()
+        phone_number = response.xpath('//div[@class="phoneNumber"]/text()').extract()
+        if phone_number:
+            trip_item['PhoneNumber'] = phone_number[0].split(":")[1].strip()
+
+        #
+        #
+        div = response.xpath("//div[@class='slim_ranking']").extract()
+        if div:
             sel = Selector(text=div[0])
-            trip_item['rank'] = sel.xpath("string(//div[1])").extract_first().strip()
+            trip_item['Ranking'] = sel.xpath("string(//div[1])").extract_first().strip()
+        else:
+            trip_item['Ranking'] = ""
 
-            trip_item['phone'] = response.xpath('//div[@class="phoneNumber"]/text()').extract()[0][14:]#slicing to extract only number
+        len_fee = response.xpath("//div[@class='details_wrapper']/div[@class='detail']/text()").extract()
 
-            yield trip_item
+        if len_fee:
+            trip_item['LengthOfVisit'] = len_fee[-3].strip()
+            trip_item['Fee'] = len_fee[-1].strip()
+        else:
+            trip_item['LengthOfVisit'] = ""
+            trip_item['Fee'] = ""
 
+        trip_item['Description'] = response.xpath("//div[@class='listing_details']/p/text()").extract()
+        # trip_item['AverageRating'] = response.xpath("//div[@class='valueCount fr part']/text()")[2].extract()
+        rating = response.xpath("//div[@class='valueCount fr part']/text()").extract()
+        if rating:
+            trip_item['AverageRating'] = rating[2]
+        else:
+            trip_item['AverageRating'] = ""
 
+        email_div = response.xpath("//div[@class='taLnk fl']/@onclick").extract()
 
+        if len(email_div) > 0:
+            email = email_div[0].split(",")
+            trip_item['Email'] = email[6].strip("/'")
+        else:
+            trip_item['Email'] = ""
 
+        days = [d.strip() for d in response.xpath("//span[@class='days']/text()").extract() if d]
+        hours = [h.strip() for h in response.xpath("//span[@class='hours']/text()").extract() if h]
+        days_hours = dict(zip(days, hours))
+        trip_item['OpeningHours'] = ""
+        for day, hour in days_hours.items():
+            trip_item['OpeningHours'] += day + " " + hour + " , "
 
-
-
-
+        yield trip_item
 
